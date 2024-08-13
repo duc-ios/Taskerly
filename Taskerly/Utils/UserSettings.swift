8b73194243001042f631176929e2c00670a64321
Duc iOS <duc.nh2012@gmail.com>
2024-08-13 19:44:54 +0700

    refactor: using AppStorage for UserSettings

--- Taskerly/Utils/UserSettings.swift
+++ Taskerly/Utils/UserSettings.swift
@@ -5,29 +5,8 @@
 //  Created by Duc on 11/8/24.
 //
 
-import Foundation
-
-@propertyWrapper
-struct UserDefault<T> {
-    let key: String
-    let defaultValue: T
-
-    init(_ key: String, defaultValue: T) {
-        self.key = key
-        self.defaultValue = defaultValue
-    }
-
-    var wrappedValue: T {
-        get {
-            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: key)
-        }
-    }
-}
+import SwiftUI
 
 enum UserSettings {
-    @UserDefault("isOnboarded", defaultValue: false)
-    static var isOnboarded: Bool
+    @AppStorage("isOnboarded") static var isOnboarded: Bool = false
 }
